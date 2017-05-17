e76028f970cd418b4d414b300bd409ee85776a00
John Eargle <jeargle@gmail.com>
2017-05-16 23:20:29 -0500

    added initial program files

---
+++ src/tiledrevert.jl
@@ -0,0 +1,21 @@
+# John Eargle (mailto: jeargle at gmail.com)
+# 2017
+# tiledrevert
+
+module TiledRevert
+
+export tiledrevert
+
+
+# =======================================
+# 
+# =======================================
+
+
+# Revert Tiled output files to previous format
+# infile: Tiled file
+function tiledrevert(infile)
+end
+
+
+end
---
+++ test/test.jl
@@ -0,0 +1,8 @@
+# John Eargle (mailto: jeargle at gmail.com)
+# 2017
+# test
+
+using TiledRevert
+
+# TiledRevert
+println("\n*** TiledRevert ***\n")
