22e41b313029bd9169e04756055afc028a0421f9
John Eargle <jeargle@gmail.com>
2017-05-20 20:03:58 -0500

    got Labz decompression working for base64 string

--- test/test.jl
+++ test/test.jl
@@ -2,7 +2,24 @@
 # 2017
 # test
 
-using TiledRevert
+using Libz
+
+# using TiledRevert
+
 
 # TiledRevert
 println("\n*** TiledRevert ***\n")
+
+height = 3
+width = 11
+dataStr = "eJxjYmBgYARiJiSaCYnPiMbHJc6IQx5dLbo9IDYADewAMw=="
+print(dataStr)
+print("\n")
+data = base64decode(dataStr)
+tilemap = Libz.inflate(data)
+
+tilemap2 = reshape(tilemap, 4, width*height)
+
+tilemap3 = transpose(reshape(tilemap2[1,:], width, height))
+print(tilemap3)
+print("\n")
