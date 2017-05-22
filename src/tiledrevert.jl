5a9abc9ecb3d65cdf71c55d41152edd2ae0566af
John Eargle <jeargle@gmail.com>
2017-05-21 23:09:24 -0500

    read in JSON file and extract base64 encoded datastring

--- src/tiledrevert.jl
+++ src/tiledrevert.jl
@@ -4,7 +4,10 @@
 
 module TiledRevert
 
-export tiledrevert
+export extractTilemap, tiledRevert
+
+using JSON
+using Libz
 
 
 # =======================================
@@ -12,9 +15,42 @@ export tiledrevert
 # =======================================
 
 
+# Convert base64 encoded string into tilemap
+function extractTilemap(dataStr, height, width)
+    print(dataStr)
+    print("\n")
+    data = base64decode(dataStr)
+    tilemap = Libz.inflate(data)
+
+    tilemap2 = reshape(tilemap, 4, width*height)
+
+    tilemap3 = transpose(reshape(tilemap2[1,:], width, height))
+    return tilemap3
+end
+
+
 # Revert Tiled output files to previous format
 # infile: Tiled file
-function tiledrevert(infile)
+function tiledRevert(infile)
+    tiled = JSON.parsefile(infile)
+    # println(tiled["layers"])
+
+    layer1 = tiled["layers"][1]
+    dataStr = layer1["data"]
+    height = layer1["height"]
+    width = layer1["width"]
+    tilemap = extractTilemap(dataStr, height, width)
+    print(tilemap)
+    print("\n")
+    
+    # outfile = "outfile.json"
+    # # writing to files is very similar:
+    # f = open(outfile, "w")
+    # # both print and println can be used as usual but with f as their first arugment
+    # println(f, "some content")
+    # print(f, "more content")
+    # print(f, " more on the same line")
+    # close(f)
 end
 
 
--- test/test.jl
+++ test/test.jl
@@ -2,24 +2,10 @@
 # 2017
 # test
 
-using Libz
-
-# using TiledRevert
+using TiledRevert
 
 
 # TiledRevert
 println("\n*** TiledRevert ***\n")
 
-height = 3
-width = 11
-dataStr = "eJxjYmBgYARiJiSaCYnPiMbHJc6IQx5dLbo9IDYADewAMw=="
-print(dataStr)
-print("\n")
-data = base64decode(dataStr)
-tilemap = Libz.inflate(data)
-
-tilemap2 = reshape(tilemap, 4, width*height)
-
-tilemap3 = transpose(reshape(tilemap2[1,:], width, height))
-print(tilemap3)
-print("\n")
+tiledRevert("../assets/yes.json")
